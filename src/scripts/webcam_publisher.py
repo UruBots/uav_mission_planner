da7b32ad81d8676a28583597a4d0fb3099fa427b
Sebastian Barcelona <sbarcelona@gmail.com>
2024-07-18 17:55:52 -0300

    wip mission

--- src/scripts/webcam_publisher.py
+++ src/scripts/webcam_publisher.py
@@ -9,7 +9,7 @@ class ImagePublisher:
     def __init__(self):
         rospy.init_node('image_publisher', anonymous=True)
         self.bridge = CvBridge()
-        self.camera1 = rospy.Subscriber("/zed/zed_node/left/image_rect_color", Image, self.callbackZed)
+        # self.camera1 = rospy.Subscriber("/zed/zed_node/left/image_rect_color", Image, self.callbackZed)
         self.camera0 = cv2.VideoCapture(0)
         self.pub_cam0 = rospy.Publisher('/camera_0/rgb', Image, queue_size=10)
         self.pub_cam1 = rospy.Publisher('/camera_1/rgb', Image, queue_size=10)
@@ -17,16 +17,16 @@ class ImagePublisher:
         # self.bgr8pub = rospy.Publisher('/camera_1/bgr', Image, queue_size=10)
         # self.mono8pub = rospy.Publisher('/camera_1/mono', Image, queue_size=10)
         
-    def callbackZed(self, data):
-        try:
-            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-        except CvBridgeError as e:
-            rospy.logerr(e)
-            return
+    # def callbackZed(self, data):
+    #     try:
+    #         cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+    #     except CvBridgeError as e:
+    #         rospy.logerr(e)
+    #         return
         
-        # Process the image using OpenCV (e.g., display it)
-        cv2.imshow("Image from ZED Camera", cv_image)
-        cv2.waitKey(3)
+    #     # Process the image using OpenCV (e.g., display it)
+    #     cv2.imshow("Image from ZED Camera", cv_image)
+    #     cv2.waitKey(3)
         
 
     def run(self):
@@ -36,11 +36,11 @@ class ImagePublisher:
                 # z, frame_zed = self.camera1.read()
                 if not r:
                     return
-                self.camera0.publish(self.bridge.cv2_to_imgmsg(frame, "bgr8"))
+                self.pub_cam0.publish(self.bridge.cv2_to_imgmsg(frame, "bgr8"))
 
                 # if not z:
                 #     return
-                # self.camera1.publish(self.bridge.cv2_to_imgmsg(frame_zed, "bgr8"))
+                # self.pub_cam1.publish(self.bridge.cv2_to_imgmsg(frame_zed, "bgr8"))
                 
                 # # BGR8
                 # self.bgr8pub.publish(self.bridge.cv2_to_imgmsg(frame, "bgr8"))
@@ -55,7 +55,7 @@ class ImagePublisher:
 
             except CvBridgeError as e:
                 print(e)
-                self.cap.release()
+                self.camera0.release()
 
 def main(args=None):
     ip = ImagePublisher()
