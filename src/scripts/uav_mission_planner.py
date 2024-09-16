711517665560bc7f4d3cc53f45f800f72fe98f4a
Sebastian Barcelona <sbarcelona@gmail.com>
2024-09-16 20:51:30 -0300

    added code tested on fira

--- src/scripts/uav_mission_planner.py
+++ src/scripts/uav_mission_planner.py
@@ -7,6 +7,8 @@ from mavros_msgs.srv import *
 from mavros_msgs.msg import *
 from geographic_msgs.msg import *
 from geometry_msgs.msg import *
+#from cv_bridge import CvBridge, CvBridgeError
+
 #global variable
 latitude = 0.0
 longitude = 0.0
@@ -15,18 +17,66 @@ global local_position
 local_position = None
 global pose_x
 global pose_y
-global distanceQR
-distanceQR = 1.25
+global distanceQRy
+global distanceQRx
+distanceQRx = 1.25
+distanceQRy = 1.38
 global mission
 global pose
+pose = PoseStamped()
+global base_pose
+base_pose = PoseStamped()
+global target_flag
+target_flag = 0
+global latest_qr
+latest_qr = None
+#bridge = CvBridge()
 
-set_point_pub = rospy.Publisher("/mavros/setpoint_position/local", PoseStamped, queue_size=10)
-rospy.Subscriber("/mavros/local_position/pose", PoseStamped, local_position_callback)
 
 def local_position_callback(data):
-    local_position_pose = data
-    print(local_position_pose)
-    
+    global local_position
+    local_position = data
+    #print("#### local_position_callback updated ###", local_position)
+
+#def image_callback(data):
+#    try:
+#        cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
+#    except CvBridgeError as e:
+#        rospy.logerr(e)
+#        return
+
+#    result_image, detection = process_image(cv_image)
+
+#    if detection:
+#        rospy.loginfo("H detected at: %s", detection)
+        # this is for land
+#        setLandMode()
+
+#    try:
+#        self.image_pub.publish(bridge.cv2_to_imgmsg(result_image, "bgr8"))
+#    except CvBridgeError as e:
+#       rospy.logerr(e)
+
+def process_image(cv_image):
+    hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, self.lower_blue, self.upper_blue)
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        epsilon = 0.02 * cv2.arcLength(contour, True)
+        approx = cv2.approxPolyDP(contour, epsilon, True)
+        if len(approx) == 12:  # Assuming H shape approximates to 12 points
+            x, y, w, h = cv2.boundingRect(contour)
+            cv2.rectangle(cv_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.putText(cv_image, "H", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+            return cv_image, (x, y, w, h)
+
+    return cv_image, None
+
+set_point_pub = rospy.Publisher("/mavros/setpoint_position/local", PoseStamped, queue_size=10)
+rospy.Subscriber("/mavros/vision_pose/pose", PoseStamped, local_position_callback)
+#rospy.Subscriber('/camera_1', Image, image_callback)
+
 def call_set_mode(mode, mode_ID):
     try:
         service = rospy.ServiceProxy("/mavros/set_mode", SetMode)
@@ -98,7 +148,7 @@ def setTakeoffMode():
     rospy.wait_for_service('/mavros/cmd/takeoff')
     try:
         takeoffService = rospy.ServiceProxy('/mavros/cmd/takeoff', mavros_msgs.srv.CommandTOL) 
-        takeoffService(altitude = 0.5, latitude = 0, longitude = 0, min_pitch = 0, yaw = 0)
+        takeoffService(altitude = 1.0, latitude = 0, longitude = 0, min_pitch = 0, yaw = 0)
     except rospy.ServiceException as e:
         print("Service takeoff call failed: %s" % e)
 
@@ -110,63 +160,103 @@ def globalPositionCallback(globalPositionCallback):
     #print ("longitude: %.7f" %longitude)
     #print ("latitude: %.7f" %latitude)
 
-def set_target_position(x,y,z,w=1):
-    pose = PoseStamped()
-    pose.pose.position.x = x
-    pose.pose.position.y = y
-    pose.pose.position.z = z
-    pose.pose.orientation.x = 0
-    pose.pose.orientation.y = 0
-    pose.pose.orientation.z = 0
-    pose.pose.orientation.w = w
+def set_target_position(what, x,y,z=0.0,w=0.0):
+    global pose
+    pose_local = PoseStamped()
+    #pose.pose.position.x = x
+    #pose.pose.position.y = y
+    #pose.pose.position.z = z
+    #pose.pose.orientation.x = 0
+    #pose.pose.orientation.y = 0
+    #pose.pose.orientation.z = 0
+    #pose.pose.orientation.w = w
+    step_x = 0
+    step_y = 0
 
+    pose_local.pose.position.x = what.pose.position.x
+    pose_local.pose.position.y = what.pose.position.y
+    #print(pose_local)
     try:
-        set_point_pub.publish(pose)
+        distance_x = pose_local.pose.position.x - x
+        distance_y = pose_local.pose.position.y - y
+        print("distance")
+        print(distance_x, distance_y)
+        print("---------")
+        try:
+              while not rospy.is_shutdown():
+                   if distance_x < -0.1: step_x = 0.1
+                   elif distance_x > 0.1: step_x = -0.1
+                   else: step_x = 0.0
+
+                   if distance_y < -0.1: step_y = 0.1
+                   elif distance_y > 0.1: step_y = -0.1
+                   else: step_y = 0.0
+
+                   pose_local.pose.position.x = pose_local.pose.position.x + step_x
+                   pose_local.pose.position.y = pose_local.pose.position.y + step_y
+                   print(pose_local.pose.position.x, pose_local.pose.position.y)
+                   if((abs(pose_local.pose.position.x - x) < 0.15) and (abs(pose_local.pose.position.y - y) < 0.15)):
+                        break
+
+                   try:
+                        qr_detected = rospy.wait_for_message('/qrcode/raw', String, timeout=1.0)
+                        print("QR not detected")
+                        break
+                   except:
+                        pass
+                   try:
+                        set_point_pub.publish(pose_local)
+                        time.sleep(1)
+                   except KeyboardInterrupt:
+                        setLandMode()
+                        print("Shutting down")
+              base_pose = pose_local
+        except KeyboardInterrupt:
+              setLandMode()
+              print("Shutting down")
+
     except rospy.ServiceException as e:
         print("Service set_target_position call failed: %s" % e)
 
 def follow_line():
     pass
 
-def string_to_pose(self, input_string):
+def string_to_pose(input_string):
     global mission
     global pose
-
+    global distanceQRx
+    global distanceQRy
+    #print("#### input_string  ###", input_string)
     #TODO replace data from string 
-    parts = input_string.replace('data: ', '').replace('"', '').split(',')
+    parts = str(input_string).replace('data: ', '').replace('"', '').split(',')
+
+    print("#### parts ###", parts)
     if len(parts) != 5:
         raise ValueError("Input string should have 5 comma-separated values")
-
-    if (parts[-1] == 0):
+    if (parts[-1] == '0'):
         # Asignar la orientacion basada en la direccion de la secuencia
         if parts[mission-1] == 'N':
-            pose.pose.position.x += 1.0
-            pose.pose.position.y += 0.0           
+            pose.pose.position.x += distanceQRx
+            pose.pose.position.y += 0.0
         elif parts[mission-1] == 'E':
             pose.pose.position.x += 0.0
-            pose.pose.position.y += -1.0
+            pose.pose.position.y += -distanceQRy
         elif parts[mission-1] == 'S':
-            pose.pose.position.x += -1.0
+            pose.pose.position.x += -distanceQRx
             pose.pose.position.y += 0.0
         elif parts[mission-1] == 'W':
             pose.pose.position.x += 0.0
-            pose.pose.position.y += 1.0
-            
-        rospy.loginfo("string_to_pose -> %s" % pose)
+            pose.pose.position.y += distanceQRy
+
+        #rospy.loginfo("string_to_pose -> %s" % pose)
         # Asignar el numero de posicion objetivo
         #pose.header.stamp = rospy.Time.now()
         #pose.header.frame_id = 'map'  # Ajustar el frame_id segun sea necesario
         #pose.pose.position.z = float(parts[4])
 
-        return pose
+        return pose, parts[4]
 
-def go_to_destination(dest = "1.0, 0.0, 1.0, 0.0"):
-    global pose
-    global distanceQR
-    global mission
-    mission = 1
-    pose = PoseStamped()
-        
+def go_to_destination(dest = "1.0, 0.0, 1.0, 0.0"):        
     x, y, z, w = dest.split(",")
     setGuidedMode()
     time.sleep(1)
@@ -184,8 +274,16 @@ def go_to_destination(dest = "1.0, 0.0, 1.0, 0.0"):
     time.sleep(1)
     setDisarm()
 
-def read_qr_and_go_to_destination():
+def read_qr_and_go_to_destination(direction):
     global mission
+    global pose
+    global local_position
+    global target_flag
+    global latest_qr
+    global altitude
+    global distanceQRx
+    global distanceQRy
+    global base_pose
     mission = 1
     try:
         if (local_position != None):
@@ -208,21 +306,81 @@ def read_qr_and_go_to_destination():
             rospy.loginfo("take off")
             time.sleep(5)
             # 1 meter to the rgight
-            set_target_position(0.0,-distanceQR,0.0)
-            # set_target_position(0.0,1.0,0.0)
-            time.sleep(5)
+            print("### GET OVER THE QR CODE")
+            base_pose.pose.position.x = 0
+            base_pose.pose.position.y = 0
+            #set_target_position(base_pose, 0.0, -2.2, 0.0)
+            if(direction == 'right'):
+                  set_target_position(base_pose, 0.0, -8.0, 0.0)
+            else:
+                  set_target_position(base_pose, 0.0, 8.0, 0.0)
+            #set_target_position(0.0,1.0,0.0)
+            time.sleep(1.5)
+            #base_pose.pose.position.x = 0
+            #base_pose.pose.position.y = -7.2
+
+            #set_target_position(base_pose, 1.25, -7.2, 0.0)
+            #time.sleep(1.5)
+            #print("Going down")
+            #set_target_position(distanceQRx, 0.0, -0.5)
             # set_target_position(0.0, 2.0, 0.0)
-            # time.sleep(5)
+            #time.sleep(10)
             # set_target_position(0.0, 3.0, 0.0)
             # time.sleep(5)
-            qr_detected = rospy.wait_for_message('/qrcode/raw', String, timeout=5)
-            if(qr_detected):
-                rospy.loginfo("QR Code detected", qr_detected)
-                string_to_pose(qr_detected)
-            else:
-                rospy.loginfo("QR Code not detected")
-                setLandMode()
+
+
+            while True:
+                rospy.loginfo("### WHILE ###")
+                try:
+                    qr_detected = rospy.wait_for_message('/qrcode/raw', String, timeout=10)
+                except:
+                    qr_detected = None
+                    rospy.loginfo("### Exception on QR_DETECTED ###")
+                    break
+
+                if(qr_detected):
+                    latest_qr = qr_detected
+                    print("QR Code detected", qr_detected)
+                    print("#### BASE POSITION BEFORE ->", base_pose.pose.position.x, base_pose.pose.position.y)
+                    #rospy.loginfo("QR Code detected", qr_detected)
+                    pose, target_flag = string_to_pose(qr_detected)
+                else:
+                    rospy.loginfo("QR Code not detected")
+                    # setLandMode()
+                break
+                print("#### BASE POSITION ->", base_pose.pose.position.x, base_pose.pose.position.y)
+                print("#### GOING NEXT POSITION ->", pose.pose.position.x, pose.pose.position.y)
+                set_target_position(base_pose, pose.pose.position.x, pose.pose.position.y)
+                base_pose = pose
+                #print("#### pose ->", pose)
+                #print("#### GOING NEXT POSITION ->", pose.pose.position.x, pose.pose.position.y)
+                #time.sleep(10)
+
+                if (target_flag == 1): mission = 2
+                if (target_flag == 2): mission = 3
+                if (target_flag == 3): mission = 4
+                if (target_flag == 4): break
+
             # read qr codes with node.
+            new_pose = PoseStamped()
+            new_pose.pose.position.x = 0
+            new_pose.pose.position.y = -7.2
+            time.sleep(5)
+            try:
+                qr_detected = rospy.wait_for_message('/qrcode/raw', String, timeout=10)
+            except:
+                qr_detected = None
+                setLandMode()
+                rospy.loginfo("### Exception on QR_DETECTED ###")
+
+            set_target_position(new_pose, 1.25, -7.2)
+            time.sleep(1.5)
+            try:
+                qr_detected = rospy.wait_for_message('/qrcode/raw', String, timeout=10)
+            except:
+                qr_detected = None
+                rospy.loginfo("### Exception on QR_DETECTED ###")
+
             setLandMode()
     except rospy.ServiceException as e:
         rospy.loginfo("Exception read_qr_and_go_to_destination", e)
@@ -240,6 +398,7 @@ def menu():
     print("8: Go to destination")
     print("9: Read QR-Codes")
     print("10: fakegps")
+    print("11: Read QR-Codes L")
 
 def myLoop():
     x='1'
@@ -268,10 +427,12 @@ def myLoop():
             dest = "1.0,0.0,1.0,0.3"
             go_to_destination(dest)
         elif(x=='9'):
-            read_qr_and_go_to_destination()
+            read_qr_and_go_to_destination("right")
         elif(x=='10'):
             pub_reset_gps()
-        else: 
+        elif(x=='11'):
+            read_qr_and_go_to_destination("left")
+        else:
             print("Exit")
 
 
@@ -284,5 +445,5 @@ if __name__ == '__main__':
         myLoop()
         # rospy.spin()
     except rospy.ROSInterruptException:
+        setLandMode()
         rospy.loginfo("#### Exception uav ####")
-
