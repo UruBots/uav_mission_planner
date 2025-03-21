07e141438f6d640ae2bdfe12f9c1ead76c14113b
Sebastian Barcelona <sbarcelona@gmail.com>
2025-03-20 22:35:51 -0300

    adding a remapping for zed pose to mavros

---
+++ src/scripts/remap_pose.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import rospy
+from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped
+from nav_msgs.msg import Odometry
+from mavros_msgs.msg import State
+
+# Global variables to store the current state and odometry data
+px4_current_state = State()
+zed_current_odom = Odometry()
+
+def px4_state_cb(msg):
+    """ Callback function for PX4 state updates """
+    global px4_current_state
+    px4_current_state = msg
+
+def zed_odom_cb(msg):
+    """ Callback function for ZED odometry updates """
+    global zed_current_odom
+    zed_current_odom = msg
+
+def main():
+    rospy.init_node("remap_pose_node", anonymous=True)
+
+    # Subscribers
+    rospy.Subscriber("/mavros/state", State, px4_state_cb)
+    rospy.Subscriber("/zedm/zed_node/pose", PoseStamped, zed_odom_cb)  # Now uses /zedm/zed_node/pose
+    rospy.Subscriber("/zedm/zed_node/pose_with_covariance", PoseWithCovarianceStamped, zed_odom_cb)  # New subscriber
+
+    # Publishers
+    vision_pose_pub = rospy.Publisher("/mavros/vision_pose/pose_cov", PoseWithCovarianceStamped, queue_size=1)
+    vision_pose_pub_pose = rospy.Publisher("/mavros/vision_pose/pose", PoseStamped, queue_size=1)  # New publisher
+
+    # Sleep for 15 seconds to allow startup time
+    rospy.sleep(15)
+
+    # Set loop rate 45 Hz (45 times per second)
+    rate = rospy.Rate(45)
+
+    # Wait until PX4 is connected
+    while not rospy.is_shutdown() and not px4_current_state.connected:
+        rospy.sleep(0.1)
+
+    rospy.loginfo("Start vision pose information message!")
+
+    while not rospy.is_shutdown():
+        # Create PoseWithCovarianceStamped message
+        cur_pose_cov = PoseWithCovarianceStamped()
+        cur_pose_cov.header.frame_id = "odom"
+        cur_pose_cov.header.stamp = rospy.Time.now()
+        cur_pose_cov.pose = zed_current_odom.pose
+
+        # Create PoseStamped message
+        cur_pose = PoseStamped()
+        cur_pose.header.frame_id = "odom"
+        cur_pose.header.stamp = rospy.Time.now()
+        cur_pose.pose = zed_current_odom.pose.pose  # Adjusting for PoseStamped format
+
+        # Publish messages
+        vision_pose_pub.publish(cur_pose_cov)
+        vision_pose_pub_pose.publish(cur_pose)
+
+        rospy.loginfo("Published remapped poses.", cur_pose_cov, cur_pose)
+
+        # Maintain loop rate
+        rate.sleep()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        rospy.logerr("ROS Remapping Node interrupted.")
